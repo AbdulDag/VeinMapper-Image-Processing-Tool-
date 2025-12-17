8be0d81f60f5e0d24b7090c6d776214ccbefe869
AbdulDag <Dagabdoo@gmail.com>
2025-12-17 14:40:10 -0500

    kaggle os commit 1

--- vein-tracker.py
+++ vein-tracker.py
@@ -1,13 +1,40 @@
-import cv2
+import cv2 #OpenCV for video capture and display
+import mediapipe as mp # For hand tracking 
+import os 
+from mediapipe.tasks import python # Import python API for MediaPipe tasks
+from mediapipe.tasks.python import vision # Vision tasks 
 
 
-
-cap = cv2.VideoCapture(0)
-
-Frame_width = int(cap.get(3))
-Frame_height = int(cap.get(4))
+#setup mediapipe hand tracking model
+model_path = '/absolute/path/to/gesture_recognizer.task'
 
 
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a hand landmarker instance with the live stream mode:
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+with HandLandmarker.create_from_options(options) as landmarker:
+  # The landmarker is initialized. Use it here.
+
+
+#open cv stuff - Initializ webcam capture 
+cap = cv2.VideoCapture(0) 
+
+# Getting dimensions of the video frames 
+Frame_width = int(cap.get(3)) # Width of the frame 
+Frame_height = int(cap.get(4)) # Height of the frame
+
+# Main video processing loop 
 while True: 
     ret, frame = cap.read()
     if not ret:
